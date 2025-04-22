35bdc85219a827079408f3bfceb5f19377783bf8
Danil <128593698+nlbeglov@users.noreply.github.com>
2025-04-22 11:32:21 +0300

    Update UserController.java

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,67 +1,31 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import web.model.User;
 import web.service.UserService;
 
-import javax.validation.Valid;
-import java.util.List;
-
 @Controller
-@RequestMapping("/")
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
 
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
-    public String showAllUsers(Model model) {
-        List<User> users = userService.getAllUsers();
-        model.addAttribute("users", users);
-        System.out.println(users);
-        return "users";
-    }
-
-    @GetMapping("/addNewUser")
-    public String addUser(Model model) {
-        model.addAttribute("user", new User());
-        return "addUser";
-    }
-
-    @PostMapping("/saveUser")
-    public String create(@Valid @ModelAttribute("user") User user, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            // Если есть ошибки, возвращаем на форму добавления с ошибками
-            return "addUser";
-        }
-        userService.save(user);
-        return "redirect:/users";
-    }
-
-    @GetMapping("/updateInfo")
-    public String updateUser(@RequestParam("userId") int id, Model model) {
-        model.addAttribute("user", userService.getUserById(id));
-        return "addUser";
-    }
-
-    @PostMapping("/updateUser")
-    public String update(@Valid @ModelAttribute("user") User user, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "addUser"; // Возвращаем на форму обновления, если есть ошибки
-        }
-        userService.save(user);
-        return "redirect:/users";
-    }
-
-    @RequestMapping("/deleteUser")
-    public String deleteUser(@RequestParam("userId") int id) {
-        userService.delete(id);
-        return "redirect:/users";
+    @GetMapping
+    public String showUserInfo(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.getUserByUsername(auth.getName());
+        model.addAttribute("user", user);
+        return "user/userInfo";
     }
 }
